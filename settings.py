65bfb07b85eaa6b922a2ad1713c68bf80462e82f
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-11 06:24:30 -0500

    Update settings.py
    starting project over

--- settings.py
+++ settings.py
@@ -1,12 +1,11 @@
 class Settings:
-	'''a class to store all settings for Alien Invasion'''
 
-	def __init__(self):
-		'''initialize the game's settings'''
-		#screen settings
-		self.screen_width = 1200
-		self.screen_height = 800
-		self.bg_color = (230, 230, 230)
+    # A class to store all settings for Alien Invasion
 
-		#ship settings
-		self.ship_speed = 1.5
+    def __init__(self):
+        # Initializes the game's settings
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+	    
