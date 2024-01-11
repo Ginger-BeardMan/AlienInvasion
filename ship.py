ac5e1a3cfd68519d535f4e40272bec09d52746ec
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-11 06:25:04 -0500

    Update ship.py
    starting project over

--- ship.py
+++ ship.py
@@ -1,40 +1,20 @@
 import pygame
 
+
 class Ship:
-	'''a class to manage the ship'''
+    def __init__(self, ai_game):
+        # initialize the ship and set its starting position
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
-	def __init__(self, ai_game):
-		'''initialize the ship and set its starting position'''
-		self.screen = ai_game.screen
-		self.settings = ai_game.settings
-		self.screen_rect = ai_game.screen.get_rect()
+        # load the ship image and get its rectangle
+        self.image = pygame.image.load('../images/ship.bmp')
+        self.rect = self.image.get_rect()
 
-		#load the ship image and get its rect
-		self.image = pygame.image.load('images/ship.bmp')
-		self.rect = self.image.get_rect()
+        # start each new ship at the bottom center of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
 
-		#start each new ship at the bottom center of the screen
-		self.rect.midbottom = self.screen_rect.midbottom
-
-		#store a float for the ship's exact horizontal position
-		self.x = float(self.rect.x)
-
-		#movement flag; start with a ship that's not moving
-		self.moving_right = False
-		self.moving_left = False
-
-	def update(self):
-		'''update the ship's position based on the movement flag'''
-		''' update the ship's x value, not rect'''
-		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.x += self.settings.ship_speed
-		
-		if self.moving_left and self.rect.left > 0:
-			self.x -= self.settings.ship_speed
-
-		#update rect object from self.x
-		self.rect.x = self.x
-
-	def blitme(self):
-		'''draw the ship at its current location'''
-		self.screen.blit(self.image, self.rect)
+    def show_ship(self):
+        # draw the ship at its current location
+        self.screen.blit(self.image, self.rect)
+	    
