c835821b76932d472861be98729f38a4d6c8bb71
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-13 19:11:29 -0500

    Update alien_invasion.py
    refactoring code

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,32 +10,32 @@ class AlienInvasion:
     def __init__(self):
         # Initialize the game, and create game resources
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invasion')
-
-        # Set background color
-        self.bg_color = (230, 230, 230)
-
-        self.clock = pygame.time.Clock()
 
         self.ship = Ship(self)
 
     def run_game(self):
         # Start the main loop for the game
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color)
-
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+            
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
-            self.ship.show_ship()
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color)
+        self.ship.show_ship()
 
 
 if __name__ == '__main__':
