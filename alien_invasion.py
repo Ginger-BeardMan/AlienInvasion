623466bda2933fa518332225bb029981d9cc99d3
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-15 20:51:53 -0500

    Update alien_invasion.py
    added movement ability for the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,22 +22,38 @@ class AlienInvasion:
         # Start the main loop for the game
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
-            
+
     def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        # Make the most recently drawn screen visible
-        pygame.display.flip()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the left
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop
         self.screen.fill(self.settings.bg_color)
         self.ship.show_ship()
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
+    
