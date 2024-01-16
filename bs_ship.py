8089b4f0621797cfebf97c4435508a42f52c432c
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-15 20:52:35 -0500

    Update bs_ship.py
    added movement ability for the ship

--- bs_ship.py
+++ bs_ship.py
@@ -15,10 +15,21 @@ class Ship:
         self.rect = self.image.get_rect()
 
         self.set_location()
+        # Movement flag; start with a ship that is no moving
+        self.moving_right = False
+
+        self.moving_left = False
 
     def set_location(self):
         # start each new ship at the bottom center of the screen
         self.rect.center = self.screen_rect.center
+
+    def update(self):
+        # Update the ship's position based on the movement flag
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
 
     def show_ship(self):
         # draw the ship at its current location
