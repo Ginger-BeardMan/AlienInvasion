5083370d76b8c8de2efcb7802d26e8f68622a9c6
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-17 11:09:01 -0500

    Update ship.py
    added ship, modified ship speed, modified ship location

--- ship.py
+++ ship.py
@@ -1,20 +1,42 @@
 import pygame
+from settings import Settings
 
 
 class Ship:
     def __init__(self, ai_game):
+        self.settings = Settings()
+
         # initialize the ship and set its starting position
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # load the ship image and get its rectangle
         self.image = pygame.image.load('../images/ship.bmp')
         self.rect = self.image.get_rect()
 
+        self.set_location()
+        # Movement flag; start with a ship that is no moving
+        self.moving_right = False
+
+        self.moving_left = False
+        self.x = float(self.rect.x)
+
+    def set_location(self):
         # start each new ship at the bottom center of the screen
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.center = self.screen_rect.center
+
+    def update(self):
+        # Update the ship's position based on the movement flag
+        # Update the ship's x value, not the rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def show_ship(self):
         # draw the ship at its current location
         self.screen.blit(self.image, self.rect)
-	    
