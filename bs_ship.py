3754a1a837b9e6c581d53ee395203ec298fd3310
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2024-01-22 21:02:08 -0500

    Update bs_ship.py
    added up and down movement

--- bs_ship.py
+++ bs_ship.py
@@ -18,9 +18,12 @@ class Ship:
         self.set_location()
         # Movement flag; start with a ship that is no moving
         self.moving_right = False
-
         self.moving_left = False
         self.x = float(self.rect.x)
+
+        self.moving_up = False
+        self.moving_down = False
+        self.y = float(self.rect.y)
 
     def set_location(self):
         # start each new ship at the bottom center of the screen
@@ -29,12 +32,20 @@ class Ship:
     def update(self):
         # Update the ship's position based on the movement flag
         # Update the ship's x value, not the rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        elif self.moving_left:
+
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def show_ship(self):
         # draw the ship at its current location
